714c895289abcb8f58df85850101d46f7e3539ec
Diego Gonzalez <diegom.develops@hotmail.com>
2022-09-06 21:28:30 -0400

    feat: app
    Using dotenv to manage environments variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,18 @@ import { RepresentativesModule } from './modules/representatives/module/represen
 import { ParentsModule } from './modules/parents/module/parents.module';
 import entitiesAndDtos from './entitiesAndDtos/entitiesAndDtos';
 import Endpoint from './endpoints/Endpoint';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [StudentsModule, UsersModule, RepresentativesModule, ParentsModule],
+  imports: [
+    StudentsModule,
+    UsersModule,
+    RepresentativesModule,
+    ParentsModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, ...entitiesAndDtos, Endpoint],
   exports: [...entitiesAndDtos, Endpoint],
